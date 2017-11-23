cf1271c827df596faef6d5aa015c02339782eeda
uriahrokach <uriahrokach@gmail.com>
2017-11-23 20:22:24 +0200

    Finished Climber

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/subsystems/Climber.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/subsystems/Climber.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team2212.robot.subsystems;
 
+import java.util.function.Supplier;
+
+import com.ctre.CANTalon;
+import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
 
 import edu.wpi.first.wpilibj.PIDSource;
@@ -11,10 +15,13 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Climber extends LimitedSubsystem {
 	
-	private SpeedController climberMotor;
+	private CANTalon climberMotor;
+	public static final Supplier<Double> MAX_CURRENT = ConstantHandler.addConstantDouble("MAX_CURRENT", 0.75);
+	public static final Supplier<Double> SPEED = ConstantHandler.addConstantDouble("SPEED", 0.5);
 	
-	public Climber(SpeedController climberMotor) {
+	public Climber(CANTalon climberMotor) {
 		this.climberMotor = climberMotor;
+		this.climberMotor.setInverted(true);
 	}
 
     public void initDefaultCommand() {
@@ -31,7 +38,7 @@ public class Climber extends LimitedSubsystem {
 	@Override
 	public boolean isMax() {
 		// TODO Auto-generated method stub
-		return false;
+		return (climberMotor.getOutputCurrent() >= MAX_CURRENT.get());
 	}
 
 	@Override
