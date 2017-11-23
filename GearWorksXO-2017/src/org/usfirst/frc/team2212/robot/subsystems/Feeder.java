85b2c2ed7e9c3b7a7f788f4f4b3f83b9903072fe
uriahrokach <uriahrokach@gmail.com>
2017-11-23 18:56:08 +0200

    Finishedfeeder

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/subsystems/Feeder.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/subsystems/Feeder.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team2212.robot.subsystems;
 
+import java.util.function.Supplier;
+
+import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
 
 import edu.wpi.first.wpilibj.PIDSource;
@@ -12,9 +15,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Feeder extends LimitedSubsystem {
 
 	private SpeedController feederMotor;
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+	public static final Supplier<Double> FEEDER_SPEED = ConstantHandler.addConstantDouble("FEEDER_SPEED", 0.5);
+	
+	public Feeder(SpeedController feederMotor){
+		this.feederMotor = feederMotor;
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
