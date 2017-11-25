a68f17a8dc93d4475ec8dc81ab93c66e04d58bf5
uriahrokach <uriahrokach@gmail.com>
2017-11-25 19:40:57 +0200

    Reformated file

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/subsystems/Lift.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/subsystems/Lift.java
@@ -10,52 +10,54 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.SpeedController;
 
+/**
+ * @author Itamar Rivkind
+ *
+ */
 
 public class Lift extends LimitedSubsystem {
 
-    private SpeedController motor;
-    private DigitalInput upLimit;
-    private DigitalInput downLimit;
-    private Encoder encoder;
-    public static final Supplier<Double> SPEED = ConstantHandler.addConstantDouble("Lift-SPEED", 0.7);
-    public static /*final*/ Supplier<Integer> MIDDLE; // place in encoder for putting lower gear
-    
-    public Lift(SpeedController liftMotor, DigitalInput upLimit, DigitalInput downLimit, Encoder liftEncoder){
-    	this.motor = liftMotor;
-    	this.upLimit = upLimit;
-    	this.downLimit = downLimit;
-    	this.encoder = liftEncoder;
-    }
-    
-    public LiftPosition getPosition(){
-    	if(upLimit.get())
-    		return LiftPosition.HIGH_LIMIT;
-    	if(downLimit.get())
-    		return LiftPosition.LOW_LIMIT;
-    	if(Lift.MIDDLE.get() == this.encoder.get())
-    		return LiftPosition.MIDDLE;
-    	if(this.encoder.get() < Lift.MIDDLE.get())
-    		return LiftPosition.LOW;
-    	return LiftPosition.HIGH;
-    }
-    
-    public enum LiftPosition{
-    	LOW_LIMIT (0),
-    	LOW (1),
-    	MIDDLE (2),
-    	HIGH (3),
-    	HIGH_LIMIT (4);
-    	
-    	public int index;
-    	LiftPosition(int index){
-    		this.index = index;
-    	}
-    }
-    
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	private SpeedController motor;
+	private DigitalInput upLimit;
+	private DigitalInput downLimit;
+	private Encoder encoder;
+	public static final Supplier<Double> SPEED = ConstantHandler.addConstantDouble("Lift-SPEED", 0.7);
+	public static /* final */ Supplier<Integer> MIDDLE; // place in encoder for
+														// putting lower gear
+
+	public Lift(SpeedController motor, DigitalInput upLimit, DigitalInput downLimit, Encoder encoder) {
+		this.motor = motor;
+		this.upLimit = upLimit;
+		this.downLimit = downLimit;
+		this.encoder = encoder;
+	}
+
+	public LiftPosition getPosition() {
+		if (upLimit.get())
+			return LiftPosition.HIGH_LIMIT;
+		if (downLimit.get())
+			return LiftPosition.LOW_LIMIT;
+		if (Lift.MIDDLE.get() == this.encoder.get())
+			return LiftPosition.MIDDLE;
+		if (this.encoder.get() < Lift.MIDDLE.get())
+			return LiftPosition.LOW;
+		return LiftPosition.HIGH;
+	}
+
+	public enum LiftPosition {
+		LOW_LIMIT(0), LOW(1), MIDDLE(2), HIGH(3), HIGH_LIMIT(4);
+
+		public int index;
+
+		LiftPosition(int index) {
+			this.index = index;
+		}
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 
 	@Override
 	public boolean isMin() {
@@ -78,4 +80,3 @@ public class Lift extends LimitedSubsystem {
 		motor.set(speed);
 	}
 }
-
