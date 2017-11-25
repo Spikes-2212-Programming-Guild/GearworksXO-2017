52716ae37811da0a78f3dc698dcd42c87f526fab
uriahrokach <uriahrokach@gmail.com>
2017-11-25 19:30:18 +0200

    Changed move lift to use LiftPosition enum

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/commands/MoveLift.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/commands/MoveLift.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team2212.commands;
 
 import org.usfirst.frc.team2212.robot.Robot;
 import org.usfirst.frc.team2212.subsystems.Lift;
+import org.usfirst.frc.team2212.subsystems.Lift.LiftPosition;
 
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 
@@ -10,22 +11,20 @@ import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
  */
 public class MoveLift extends MoveLimitedSubsystem {
 
-	int target;
+	LiftPosition target;
 
-	public MoveLift(int target) {
-		super(Robot.lift, (target > Robot.lift.getPosition()) ? Lift.SPEED.get() : -Lift.SPEED.get());
-		this.target = target;
+	public MoveLift(LiftPosition target) {
+		super(Robot.lift, (Robot.lift.getPosition().index < target.index) ? Lift.SPEED.get() : -Lift.SPEED.get());
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return super.isFinished() || (target == 1 && Robot.lift.liftEncoder.get() == Lift.MIDDLE.get());
+		return (target.index == Robot.lift.getPosition().index);
 	}
 
 	// Called once after isFinished returns true
 	protected void end() {
 		super.end();
-		Robot.lift.position = this.target;
 	}
 
 	// Called when another command which requires one or more of the same
