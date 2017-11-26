707b69361f0c00b622e509ea56563af00277bffb
Matan <Matan@192.168.1.104>
2017-11-26 15:30:49 +0200

    Finished GearPicker

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/subsystems/GearPicker.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/subsystems/GearPicker.java
@@ -1,45 +1,55 @@
 package org.usfirst.frc.team2212.robot.subsystems;
 
-import javax.naming.LimitExceededException;
-
 import com.spikes2212.genericsubsystems.LimitedSubsystem;
 
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class GearPicker extends LimitedSubsystem {
-	
-	private SpeedController pickerMotor;
-	private AnalogPotentiometer potentiometer;
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	private SpeedController motor;
+	private Encoder encoder;
+	private DigitalInput maxLimit;
+	private DigitalInput minLimit;
+
+	public void gearPicker(Encoder pickerEncoder, SpeedController pickerMotor, DigitalInput upLimit,DigitalInput downLimit) {
+		this.encoder = pickerEncoder;
+		this.motor = pickerMotor;
+		this.maxLimit = upLimit;
+		this.minLimit = downLimit;
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+
 	@Override
 	public PIDSource getPIDSource() {
 		// TODO Auto-generated method stub
-		return null;
+		return encoder;
 	}
+
 	@Override
 	public boolean isMax() {
 		// TODO Auto-generated method stub
-		return false;
+		return maxLimit.get();
 	}
+
 	@Override
 	public boolean isMin() {
 		// TODO Auto-generated method stub
-		return false;
+		return minLimit.get();
 	}
-	@Override
-	protected void move(double arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-}
 
+	@Override
+	protected void move(double speed) {
+		this.motor.set(speed);
+	}
+
+}
