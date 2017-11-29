fbc084f1d3f06ab938c16b421e0ff2e01622975c
Tomer Sitton <tomer.sitton1@gmail.com>
2017-11-29 06:49:10 +0200

    reformatted RobotMap.java

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/RobotMap.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/RobotMap.java
@@ -14,14 +14,14 @@ public class RobotMap {
 
 	// TODO insert correct ports
 	public interface CAN {
-		public static final int FOLDER= 0;
-
-		public static final int CLIMBER = 5;
+		public static final int FOLDER = 0;
 
 		public static final int DRIVE_LEFT_FRONT = 1;
 		public static final int DRIVE_LEFT_REAR = 2;
 		public static final int DRIVE_RIGHT_FRONT = 3;
 		public static final int DRIVE_RIGHT_REAR = 4;
+		
+		public static final int CLIMBER = 5;
 	}
 
 	// TODO insert correct ports
@@ -46,7 +46,7 @@ public class RobotMap {
 		public static final int DRIVE_LEFT_ENCODER_B = 9;
 		public static final int DRIVE_RIGHT_ENCODER_A = 10;
 		public static final int DRIVE_RIGHT_ENCODER_B = 11;
-		
+
 		public static final int ROLLER_SENSOR = 12;
 	}
 
