83be5eb9b7a41d6041437512d4d2c5ef035fc572
omri cohen <omrichoen@gmail.com>
2017-11-29 16:52:49 +0200

    added RollGearAndFold

---
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/commandGroups/RollGearAndFold.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team2212.robot.commands.commandGroups;
+
+import java.util.function.Supplier;
+
+import org.usfirst.frc.team2212.robot.Robot;
+import org.usfirst.frc.team2212.robot.commands.RollGear;
+
+import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class RollGearAndFold extends CommandGroup {
+
+	public RollGearAndFold(Supplier<Double> rollSpeed, double rollWaitTime, Supplier<Double> foldSpeed) {
+		addSequential(new RollGear(rollSpeed, rollWaitTime));
+		addSequential(new MoveLimitedSubsystem(Robot.folder, foldSpeed));
+	}
+}
