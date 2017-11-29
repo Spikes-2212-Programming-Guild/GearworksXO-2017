b14e9ecf254606c4e1abb636a4d663be4ecc0d73
omri cohen <omrichoen@gmail.com>
2017-11-29 17:01:43 +0200

    added DropGearWithAngle

---
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/commandGroups/DropGearWithAngle.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team2212.robot.commands.commandGroups;
+
+import java.util.function.Supplier;
+
+import org.usfirst.frc.team2212.robot.Robot;
+import org.usfirst.frc.team2212.robot.commands.RollGear;
+
+import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class DropGearWithAngle extends CommandGroup {
+
+    public DropGearWithAngle(Supplier<Double> foldSpeed, Supplier<Double> rollSpeed, double rollWaitTime) {
+        addParallel(new MoveLimitedSubsystem(Robot.folder, foldSpeed));
+        addParallel(new RollGear(rollSpeed, rollWaitTime));
+    }
+}
