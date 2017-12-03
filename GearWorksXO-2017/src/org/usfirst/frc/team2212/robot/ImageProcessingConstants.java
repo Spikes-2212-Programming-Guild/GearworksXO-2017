11c8bc3f9c0c750ad273d98292fb6cc3d7da4ef0
Tomer Sitton <tomer.sitton1@gmail.com>
2017-12-03 13:19:02 +0200

    added center calculations

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/ImageProcessingConstants.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/ImageProcessingConstants.java
@@ -1,5 +1,22 @@
 package org.usfirst.frc.team2212.robot;
 
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
 public class ImageProcessingConstants {
+	public static final int CAMERA_WIDTH = 640;
+	public static final int CAMERA_HEIGHT = 360;
+
+	public static final NetworkTable NETWORK_TABLE = NetworkTable.getTable("ImageProcessing");
+
+	// calculates the center of the big reflective
+	public static final Supplier<Double> BIG_OBJECT_CENTER = () -> ((NETWORK_TABLE.getNumber("x0", 0)
+			+ 0.5 * NETWORK_TABLE.getNumber("width0", 0)) / CAMERA_WIDTH - 0.5);
+	// calculates the center of the small reflective
+	public static final Supplier<Double> SMALL_OBJECT_CENTER = () -> ((NETWORK_TABLE.getNumber("x1", 0)
+			+ 0.5 * NETWORK_TABLE.getNumber("width1", 0)) / CAMERA_WIDTH - 0.5);
+	// calculates the center of the two reflectives
+	public static Supplier<Double> TWO_OBJECTS_CENTER = () -> (BIG_OBJECT_CENTER.get() + SMALL_OBJECT_CENTER.get()) / 2;
 
 }
