1a85488318f967fddc3d5e9e28d69ee72ce9d33b
Tomer Sitton <tomer.sitton1@gmail.com>
2017-12-03 13:24:15 +0200

    added documentation

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/ImageProcessingConstants.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/ImageProcessingConstants.java
@@ -4,19 +4,25 @@ import java.util.function.Supplier;
 
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
+/**
+ * A class which calculates the position of the peg by the values given from the
+ * "ImageProcessing" {@link NetworkTable} and puts it into a static
+ * {@link Supplier}
+ *
+ */
 public class ImageProcessingConstants {
 	public static final int CAMERA_WIDTH = 640;
 	public static final int CAMERA_HEIGHT = 360;
 
 	public static final NetworkTable NETWORK_TABLE = NetworkTable.getTable("ImageProcessing");
 
-	// calculates the center of the big reflective
+	// calculates the center of the big reflective on the X axis
 	public static final Supplier<Double> BIG_OBJECT_CENTER = () -> ((NETWORK_TABLE.getNumber("x0", 0)
 			+ 0.5 * NETWORK_TABLE.getNumber("width0", 0)) / CAMERA_WIDTH - 0.5);
-	// calculates the center of the small reflective
+	// calculates the center of the small reflective on the X axis
 	public static final Supplier<Double> SMALL_OBJECT_CENTER = () -> ((NETWORK_TABLE.getNumber("x1", 0)
 			+ 0.5 * NETWORK_TABLE.getNumber("width1", 0)) / CAMERA_WIDTH - 0.5);
-	// calculates the center of the two reflectives
+	// calculates the center of the two reflectives on the X axis
 	public static Supplier<Double> TWO_OBJECTS_CENTER = () -> (BIG_OBJECT_CENTER.get() + SMALL_OBJECT_CENTER.get()) / 2;
 
 }
