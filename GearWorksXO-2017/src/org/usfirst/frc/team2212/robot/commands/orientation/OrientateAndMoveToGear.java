f127dc3b26980291e2d48ae191d1370c0903477f
Tomer Sitton <tomer.sitton1@gmail.com>
2017-12-05 19:14:26 +0200

    added orientation tolerance

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/orientation/OrientateAndMoveToGear.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/orientation/OrientateAndMoveToGear.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 
 import org.usfirst.frc.team2212.robot.Robot;
 
+import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -12,6 +13,8 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class OrientateAndMoveToGear extends CommandGroup {
+
+	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("orientation-TOLERANCE", 0.05);
 
 	public OrientateAndMoveToGear(Supplier<Double> turningSpeed, Supplier<Double> forwardsSpeed) {
 		addSequential(new OrientToTwoTargets(turningSpeed));
