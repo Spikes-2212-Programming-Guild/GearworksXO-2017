f67116d763b3813f273f5fbc7f57cf8e2c787e90
Spikes-2212 <spikes.no2212@gmail.com>
2017-12-07 14:46:38 +0200

    made isFinished look better

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/MoveFolder.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/MoveFolder.java
@@ -4,28 +4,30 @@ import java.util.function.Supplier;
 
 import org.usfirst.frc.team2212.robot.Robot;
 
-import com.spikes2212.genericsubsystems.LimitedSubsystem;
 import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
 
-import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.Timer;
 
 public class MoveFolder extends MoveLimitedSubsystem {
-	
+
 	private double lastTimeNotOnTarget;
 	private double waitTime;
-	private boolean up;
 
 	public MoveFolder(Supplier<Double> speed, double waitTime) {
 		super(Robot.folder, speed);
-		up = speed.get()>0;
 		this.waitTime = waitTime;
 	}
-	
+
 	@Override
-	public boolean isFinished(){
-		if((!Robot.folder.isMax() && up) || (!Robot.folder.isMin()&& !up))
-			lastTimeNotOnTarget = Timer.getFPGATimestamp();
-		return super.isFinished() && (Timer.getFPGATimestamp() - lastTimeNotOnTarget >= waitTime);
+	protected boolean isFinished() {
+		double currentTime = Timer.getFPGATimestamp();
+		if (!super.isFinished()) {
+			lastTimeNotOnTarget = currentTime;
+		}
+		if (currentTime - lastTimeNotOnTarget > waitTime) {
+			return true;
+		}
+		return false;
 	}
+
 }
