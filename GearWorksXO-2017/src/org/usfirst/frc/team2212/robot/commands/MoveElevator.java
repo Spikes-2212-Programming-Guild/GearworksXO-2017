0703bb686a5698243c46bb6994c66a27ec71565a
Tomer Sitton <tomer.sitton1@gmail.com>
2017-12-08 10:06:25 +0200

    added TODO

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/MoveElevator.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/MoveElevator.java
@@ -12,7 +12,7 @@ public class MoveElevator extends MoveLimitedSubsystem {
 
 	//TODO - find correct tolerance
 	private int target;
-
+	// TODO - change name to MoveElevatorToTarget
 	public MoveElevator(int target) {
 		super(Robot.elevator,
 				(Robot.elevator.getPosition() < target) ? Elevator.SPEED_UP.get() : Elevator.SPEED_DOWN.get());
