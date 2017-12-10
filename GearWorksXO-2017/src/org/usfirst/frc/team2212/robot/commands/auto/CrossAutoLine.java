483040f4595cbd29c6983e8d46d25d100faf8a3a
omri cohen <omrichoen@gmail.com>
2017-12-10 20:15:28 +0200

    added CrossAutoLine

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/auto/CrossAutoLine.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/auto/CrossAutoLine.java
@@ -1,5 +1,23 @@
 package org.usfirst.frc.team2212.robot.commands.auto;
 
-public class CrossAutoLine {
+import java.util.function.Supplier;
+
+import org.usfirst.frc.team2212.robot.Robot;
+
+import com.spikes2212.dashboard.ConstantHandler;
+import com.spikes2212.genericsubsystems.drivetrains.commands.DriveArcade;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class CrossAutoLine extends CommandGroup {
+
+	public static final Supplier<Double> CROSSING_SPEED = ConstantHandler
+			.addConstantDouble("crossLineAuto - crossingSpeed", 0.4);
+	public static final Supplier<Double> CROSSING_TIME = ConstantHandler
+			.addConstantDouble("crossLineAuto - crossingTime", 5);
+
+	public CrossAutoLine() {
+		addSequential(new DriveArcade(Robot.drivetrain, CROSSING_SPEED, () -> 0.0), CROSSING_TIME.get());
+	}
 
 }
