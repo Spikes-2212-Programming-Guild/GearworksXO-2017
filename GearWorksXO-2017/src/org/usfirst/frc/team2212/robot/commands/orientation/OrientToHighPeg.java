2d983589693df9ab8062bc6d4b55cded16daf693
Tomer Sitton <tomer.sitton1@gmail.com>
2017-12-12 08:03:56 +0200

    fixed cameras ids

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/orientation/OrientToHighPeg.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/orientation/OrientToHighPeg.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team2212.robot.commands.orientation;
 import java.util.function.Supplier;
 
 import org.usfirst.frc.team2212.robot.ImageProcessingConstants;
+import org.usfirst.frc.team2212.robot.RobotMap;
 
 import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.utils.RunnableCommand;
@@ -13,14 +14,12 @@ import edu.wpi.first.wpilibj.command.WaitCommand;
  *
  */
 public class OrientToHighPeg extends CommandGroup {
-	public static final Supplier<Double> HIGH_PEG_CAMERA_ID = ConstantHandler
-			.addConstantDouble("OrientToHighPeg-CAMERA_ID", 0);
 	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("OrientToHighPeg-TOLERANCE",
 			0.025);
 
 	public OrientToHighPeg(Supplier<Double> turningSpeed) {
 		addSequential(new RunnableCommand(
-				() -> ImageProcessingConstants.NETWORK_TABLE.putNumber("currentCamera", HIGH_PEG_CAMERA_ID.get())));
+				() -> ImageProcessingConstants.NETWORK_TABLE.putNumber("currentCamera", RobotMap.USB.HIGH_CAM)));
 		addSequential(new OrientToTwoTargets(turningSpeed, TOLERANCE.get()));
 		addSequential(new WaitCommand(0.3));
 	}
--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/orientation/OrientToLowPeg.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/orientation/OrientToLowPeg.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team2212.robot.commands.orientation;
 import java.util.function.Supplier;
 
 import org.usfirst.frc.team2212.robot.ImageProcessingConstants;
+import org.usfirst.frc.team2212.robot.RobotMap;
 
 import com.spikes2212.dashboard.ConstantHandler;
 import com.spikes2212.utils.RunnableCommand;
@@ -12,14 +13,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class OrientToLowPeg extends CommandGroup {
-
-	public static final Supplier<Double> LOW_PEG_CAMERA_ID = ConstantHandler.addConstantDouble("OrientToLowPeg-CAMERA_ID",
-			1);
-	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("OrientToLowPeg-TOLERANCE", 0.05);
+	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("OrientToLowPeg-TOLERANCE",
+			0.05);
 
 	public OrientToLowPeg(Supplier<Double> turningSpeed) {
 		addSequential(new RunnableCommand(
-				() -> ImageProcessingConstants.NETWORK_TABLE.putNumber("currentCamera", LOW_PEG_CAMERA_ID.get())));
+				() -> ImageProcessingConstants.NETWORK_TABLE.putNumber("currentCamera", RobotMap.USB.LOW_CAM)));
 		addSequential(new OrientToTwoTargets(turningSpeed, TOLERANCE.get()));
 	}
 
