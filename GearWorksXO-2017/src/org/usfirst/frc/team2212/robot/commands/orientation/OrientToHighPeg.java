c486f63c253e73ac2e4d390852321815f06f1079
Tomer Sitton <tomer.sitton1@gmail.com>
2017-12-12 09:55:09 +0200

    reformated file OrientToHighPeg.java

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/orientation/OrientToHighPeg.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/orientation/OrientToHighPeg.java
@@ -11,16 +11,21 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
- *
+ * this {@link CommandGroup} orients to the high peg automatically
  */
 public class OrientToHighPeg extends CommandGroup {
+
+	// the tolerance of the orientation to the center of the peg
 	public static final Supplier<Double> TOLERANCE = ConstantHandler.addConstantDouble("OrientToHighPeg-TOLERANCE",
 			0.025);
 
 	public OrientToHighPeg(Supplier<Double> turningSpeed) {
+		
 		addSequential(new RunnableCommand(
 				() -> ImageProcessingConstants.NETWORK_TABLE.putNumber("currentCamera", RobotMap.USB.HIGH_CAM)));
+
 		addSequential(new OrientToTwoTargets(turningSpeed, TOLERANCE.get()));
+		
 		addSequential(new WaitCommand(0.3));
 	}
 }
