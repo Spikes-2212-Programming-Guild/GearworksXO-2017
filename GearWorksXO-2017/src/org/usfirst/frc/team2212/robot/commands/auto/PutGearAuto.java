af738834ba58c93d60a46b11dc824426270dcef2
Spikes-2212 <spikes.no2212@gmail.com>
2017-12-13 19:28:07 +0200

    added drive command after orientation

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/auto/PutGearAuto.java
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/auto/PutGearAuto.java
@@ -12,22 +12,25 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class PutGearAuto extends CommandGroup {
 
-	public static final Supplier<Double> DRIVE_SPEED_START = ConstantHandler.addConstantDouble("putGearAuto- start driving speed",
-			0.3);
+	public static final Supplier<Double> DRIVE_SPEED_START = ConstantHandler
+			.addConstantDouble("putGearAuto- start driving speed", 0.3);
 	public static final Supplier<Double> DRIVE_TIME = ConstantHandler.addConstantDouble("putGearAuto- driving time",
 			0.5);
 	public static final Supplier<Double> LEFT_ROTATE_SPEED = ConstantHandler
 			.addConstantDouble("putGearAuto- left rotate speed", 0.5);
 	public static final Supplier<Double> RIGHT_ROTATE_SPEED = ConstantHandler
 			.addConstantDouble("putGearAuto- right rotate speed", -0.5);
-	
-	public static final Supplier<Double> DRIVE_TO_GEAR_SPEED = ConstantHandler.addConstantDouble("putGearAuto - drive speed to gear", 0.5);
-	public static final Supplier<Double> DRIVE_TO_GEAR_TIME = ConstantHandler.addConstantDouble("putGearAuto - drive time to gear", 0.5);
+
+	public static final Supplier<Double> DRIVE_TO_GEAR_SPEED = ConstantHandler
+			.addConstantDouble("putGearAuto - drive speed to gear", 0.5);
+	public static final Supplier<Double> DRIVE_TO_GEAR_TIME = ConstantHandler
+			.addConstantDouble("putGearAuto - drive time to gear", 0.5);
 
 	public PutGearAuto(Supplier<Double> rotateSpeed) {
 		addSequential(new DriveArcade(Robot.drivetrain, DRIVE_SPEED_START, () -> 0.0), DRIVE_TIME.get());
 
 		addSequential(new OrientToLowPeg(rotateSpeed));
 
+		addSequential(new DriveArcade(Robot.drivetrain, DRIVE_TO_GEAR_SPEED, () -> 0.0), DRIVE_TO_GEAR_TIME.get());
 	}
 }
