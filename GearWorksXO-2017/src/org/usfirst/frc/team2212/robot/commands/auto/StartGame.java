5d3416a7e439662c1343af33989b4aed60266a0d
Spikes-2212 <spikes.no2212@gmail.com>
2017-12-14 18:19:30 +0200

    added StartingPreparation.java

--- GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/auto/StartGame.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team2212.robot.commands.auto;
-
-/**
- * initialize the robot's subsystems
- *
- */
-public class StartGame {
-
-}
---
+++ GearWorksXO-2017/src/org/usfirst/frc/team2212/robot/commands/auto/StartingPreparation.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team2212.robot.commands.auto;
+
+import org.usfirst.frc.team2212.robot.Robot;
+import org.usfirst.frc.team2212.robot.commands.MoveLimitedSubsystemWithTimeSinceReachingLimit;
+import org.usfirst.frc.team2212.robot.subsystems.Elevator;
+import org.usfirst.frc.team2212.robot.subsystems.Folder;
+
+import com.spikes2212.genericsubsystems.commands.MoveLimitedSubsystem;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * initialize the robot's subsystems
+ *
+ */
+public class StartingPreparation extends CommandGroup {
+
+	public StartingPreparation() {
+		addSequential(new MoveLimitedSubsystemWithTimeSinceReachingLimit(Robot.folder, Folder.SPEED_UP,
+				Folder.WAIT_TIME.get()));
+
+		addSequential(new MoveLimitedSubsystem(Robot.elevator, Elevator.SPEED_DOWN));
+	}
+
+}
